a6cfbc42f79e6c9289c73f50abc4dc8e53b8a1de
Kamily Oliveira <kamy.oliveira@outlook.com.br>
2026-03-30 22:20:46 -0300

    Add files via upload

---
+++ pilha/testePrint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    float x = 2.5;
+    int a = 10;
+    int m = printf ("oi\n");
+    printf (" m = %d\n", m);
+    int n = printf ("a = %d\n", a);
+    printf (" n = %d\n", n);
+    return 0;
+}
